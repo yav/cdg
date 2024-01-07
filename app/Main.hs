a6650348b12e7dec090180ec6ca4a508a0b167bf
Iavor Diatchki <iavor.diatchki@gmail.com>
2024-01-07 10:26:06 -0800

    Make a gloss based viwer

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,22 @@ module Main where
 import Data.ByteString qualified as BS
 import Data.ByteString.Lazy qualified as LBS
 import Data.Binary(decodeOrFail)
-import Control.Monad(foldM_,foldM)
 import System.Environment(getArgs)
-import System.Directory(createDirectoryIfMissing)
-import System.FilePath((</>),(<.>))
 import Codec.Picture qualified as J
+
+import Graphics.Gloss.Interface.Pure.Simulate qualified as G
+import Graphics.Gloss.Juicy(fromImageRGBA8)
 
 import CDG
 import CDG.Constants
 import CDG.Eval
-
 
 main :: IO ()
 main =
  do args <- getArgs
     case args of
       [file] ->
-        processFile =<< BS.readFile file
+        play =<< BS.readFile file
       _ -> putStrLn "Need a file"
 
 
@@ -38,45 +37,17 @@ test = saveState "test.png" s4
   hs = ScrollAmt 1 0
   vs = ScrollAmt 0 0
 
--- | The time at a particular packet
-timeInMilli :: Int -> Int
-timeInMilli packetNum = (1000 * packetNum) `quot` packetsPerSec
-
-showTimeMilli :: Int -> String
-showTimeMilli millis = show mins ++ ":" ++ show ss ++ ":" ++ show ms
+play :: BS.ByteString -> IO ()
+play bs = G.simulate disp G.black packetsPerSec initState seeState nextState
   where
-  (secs,ms)  = millis `quotRem` 1000
-  (mins,ss)  = secs `quotRem` 60
+  disp = G.InWindow "K"  (screenVisibleWidth,screenVisibleHeight) (0,0)
+  initState = (bs, initialState)
+  nextState _ _ (!bytes,!s) =
+    ( BS.drop 24 bytes
+    , case decodeOrFail (LBS.fromStrict bytes) of
+        Left {}         -> s
+        Right (_,_,cmd) -> doCommand cmd s
+    )
 
-processFile :: BS.ByteString -> IO ()
-processFile bs =
-  do createDirectoryIfMissing True dir
-     (_,_,is,_) <- foldM doPacket (0,0,[],initialState) (take packets [ 0 .. ])
-     let is' = map (J.convertRGB8 . J.ImageRGBA8) (reverse is)
-     case J.writeGifAnimation "x.gif" 5 J.LoopingNever is' of
-       Right ok -> ok
-       Left err -> print err
-     pure ()
-  where
-  dir           = "out"
-  outFile i     = dir </> ("frame_" ++ show i) <.> "png"
+  seeState (_,s) = fromImageRGBA8 (render s)
 
-  len           = BS.length bs
-  packets       = len `quot` packetSize
-  packetBytes i = LBS.fromStrict (BS.take packetSize (BS.drop (i * packetSize) bs))
-  doPacket (!done,lastSave,is,!s) i =
-   do -- putStrLn ("Time " ++ showTimeMilli (timeInMilli done))
-      case decodeOrFail (packetBytes i) of
-        Left {} -> pure (done + 1, lastSave, is, s)
-        Right (_,off,cmd)
-          | off /= 24 -> error $ unlines [ "didn't read all bytes"
-                                         , show off
-                                         , show cmd ]
-          | otherwise ->
-          do let s1 = doCommand cmd s
-             let t = timeInMilli done
-                 frames = (t - lastSave) `quot` 50
-             pure
-               if frames > 0
-                  then (done + 1, t, replicate frames (render s1) ++ is, s1)
-                  else (done + 1, lastSave, is, s1)
